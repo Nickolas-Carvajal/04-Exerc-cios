94482740b8e7c4753d954cc0b0069a0fa648ca8a
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-04 20:54:20 -0300

    exercicios em andamento

--- src/Exercicio01/Produto.java
+++ src/Exercicio01/Produto.java
@@ -6,14 +6,14 @@ public class Produto {
     int quantidade;
 
 
-    public Produto(String nome, double valor, int quantidade){
-        this.nome = nome;
-        this.valor = valor;
-        this.quantidade = quantidade;
-        System.out.println(this);
+        public Produto(String nome, double valor, int quantidade){
+            this.nome = nome;
+            this.valor = valor;
+            this.quantidade = quantidade;
+            System.out.println(this);
 
 
-    }
+        }
 
     public void aumentarValor(double porcentagem){
         valor *= (1+ porcentagem/100);
